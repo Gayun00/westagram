eadc8f8c2d5500255dd3f9f6a66d601b9949ae44
gy <gayunlee11@gmail.com>
2021-12-07 17:14:34 +0900

    Add: id,pw유효성 검사 완료

--- js/login.js
+++ js/login.js
@@ -3,26 +3,28 @@ const $form = document.querySelector('form');
 const $idInput = document.querySelector('#id');
 const $pwInput = document.querySelector('#password');
 const $loginButton = document.querySelector('#login-button');
+const $notice = document.querySelector('.notice');
 
-// $form.addEventListener('keydown', activeButton)
-
-// const $loginInputs = document.querySelectorAll('.login__inpug');
-// $loginInputs.forEach((loginInput) => {
-//     loginInput.addEventListener('keyup', activeButton);
-// })
 
 $idInput.addEventListener('keyup', activeButton);
 $pwInput.addEventListener('keyup', activeButton);
+
 
 function activeButton() {
     const idInputVal = $idInput.value;
     const pwInputVal = $pwInput.value;
 
     if(idInputVal.length > 0 && pwInputVal.length > 0) {
-        if(!$loginButton.classList.contains('active')) {
-            $loginButton.classList.add('active');
+        if(idInputVal.includes('@') && pwInputVal.length >= 5) {
+            if(!$loginButton.classList.contains('active')) {
+                $loginButton.classList.add('active');
+            }
+            if(!$notice.classList.contains('hide')) {
+                $notice.classList.add('hide');
+            }
         }
     } else {
         $loginButton.classList.remove('active');
+        $notice.classList.remove('hide')
     }
 }
