e7cd876ba46e1c1f7ab953d266654997dc6362e6
gy <gayunlee11@gmail.com>
2021-12-07 18:17:33 +0900

    Add: 삭제 버튼 클릭시 댓글 삭제

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const $commentInput = document.querySelector('.comment__input--text');
 const $commentButton = document.querySelector('.comment__input--button');
-const $comments = document.querySelector('.comments');
+const $commentContainer = document.querySelector('.comments');
+const $comments = document.querySelector('comment');
+const $feedButtons = document.querySelector('.feed__button');
+const $deleteButton = document.querySelector('.comment__delete-button');
+
 $commentButton.addEventListener('click', addComment);
-$commentButton.addEventListener('')
+$commentContainer.addEventListener('click', deleteComment);
+$feedButtons.addEventListener('click', handleLike);
 
 function addComment(e) {
     e.preventDefault();
@@ -10,9 +15,33 @@ function addComment(e) {
     const newCommentEl = document.createElement('ul');
     newCommentEl.classList.add('comment');
     newCommentEl.innerHTML = `
-        <span class="comment__id">canon_mj</span>
-        <span class="comment__text">${commentVal}</span>
+        <span class="comment_item">
+            <span class="comment__id">canon_mj</span>
+            <span class="comment__text">${commentVal}</span>
+        </span>
+        <button class="comment__delete-button">
+            <i class="fas fa-times"></i>
+        </button>
     `
-    $comments.appendChild(newCommentEl);
+    $commentContainer.appendChild(newCommentEl);
+    $commentInput.value = '';
 }
 
+function deleteComment(e) {
+    const target = e.target;
+    if(target.className !== 'fas fa-times') return;
+    console.log($comments)
+}
+console.log($comments)
+
+function handleLike(e) {
+    const target = e.target;
+
+    if(!target.className.includes('fa-heart')) {
+        return;
+    } else {
+        target.classList.toggle('far');
+        target.classList.toggle('fas');
+        target.classList.toggle('liked');
+    }
+}
